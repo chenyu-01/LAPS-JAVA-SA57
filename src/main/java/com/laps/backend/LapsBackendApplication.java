8db0d0e44edd320047172efd3b95fcf7f1854b24
chenyu-01 <chenyu101@outlook.com>
2023-12-06 16:17:06 +0800

    initialize

---
+++ src/main/java/com/laps/backend/LapsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.laps.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LapsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LapsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laps/backend/LapsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laps.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LapsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
