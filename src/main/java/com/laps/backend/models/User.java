e81017fe377972958d0f978fe303bd68495ae11a
chenyu-01 <chenyu101@outlook.com>
2023-12-15 00:34:27 +0800

    refactor User.java, add Validation

--- src/main/java/com/laps/backend/models/User.java
+++ src/main/java/com/laps/backend/models/User.java
@@ -2,63 +2,35 @@ package com.laps.backend.models;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Pattern;
-
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Setter;
+import lombok.Getter;
+@Setter @Getter
 @Entity
 @Table(name = "app_user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter(AccessLevel.NONE) // disable getter for id
+    @Setter(AccessLevel.NONE) // disable setter for id
     private Long id;
     // mandatory for name, role, username, password
     @Column(nullable = false)
     private String name;
-    @Pattern(regexp = "Employee|Manager|Admin")
+    @Pattern(regexp = "Employee|Manager|Admin", message = "Role must be Employee, Manager, or Admin")
     @Column(nullable = false)
     private String role; // e.g., Employee, Manager, Admin, only 3 roles
 
     @Email
     @Column(nullable = false, unique = true)
     private String email; // unique email address
+    @Size(min = 8, max = 20)
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "Password must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number")
     @Column(nullable = false)
     private String password;
 
     // Relationships e.g., OneToMany for leave applications
-    // Standard getters and setters
 
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public void setEmail(String username) {
-        this.email = username;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
 
