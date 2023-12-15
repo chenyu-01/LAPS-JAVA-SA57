5e259a057b336d718b833cae3ab90ff3b43926c1
chenyu-01 <chenyu101@outlook.com>
2023-12-15 16:07:13 +0800

    add controller to ensure redirection to index.html

---
+++ src/main/java/com/laps/backend/controllers/WebAppController.java
@@ -0,0 +1,14 @@
+package com.laps.backend.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebAppController {
+
+    // Redirect all routes to index.html
+    @RequestMapping(value = "/{path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
