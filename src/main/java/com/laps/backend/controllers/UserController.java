2eccec5597325c9471dda7a2524b835621e567c7
chenyu-01 <chenyu101@outlook.com>
2023-12-15 16:59:33 +0800

    update api to api/users

--- src/main/java/com/laps/backend/controllers/UserController.java
+++ src/main/java/com/laps/backend/controllers/UserController.java
@@ -1,22 +1,71 @@
 package com.laps.backend.controllers;
 
 import com.laps.backend.models.User;
-import com.laps.backend.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.laps.backend.models.UserDTO;
+import com.laps.backend.services.UserServiceImpl;
+
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-/*
-In the "Leave Application Processing System" project, the UserController is responsible for managing user-related functionalities. This includes creating, modifying, and deleting user accounts. The controller enables the assignment and modification of user roles (such as employee, admin, and manager). It plays a crucial role in maintaining the integrity and functionality of the user aspect of the system, which is a key component in managing leave applications, approvals, and overall system administration. For more detailed information and specific functionalities, you may refer to the document. */
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/users")
 public class UserController {
-    @Autowired
-    private UserService userService;
+    private final UserServiceImpl  userService;
 
-    // RESTful endpoints for managing users
+    public UserController(UserServiceImpl userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> credentials, HttpSession session) {
+        String email = credentials.get("email");
+        String password = credentials.get("password");
+
+        // ... handle login
+        User user = userService.findByEmail(email);
+        if (user == null || !user.getPassword().equals(password)){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password");
+        }
+
+        UserDTO userDTO = new UserDTO(user);
+        session.setAttribute("user", userDTO);
+
+
+        // Return the response entity with the cookie
+        return ResponseEntity.ok()
+                .body(userDTO);
+    }
+
+    @GetMapping("/check-auth")
+    public ResponseEntity<?> checkAuthentication(HttpSession session) {
+        UserDTO userDTO = (UserDTO) session.getAttribute("user");
+        if (userDTO != null) {
+            return ResponseEntity.ok(userDTO);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body("Unauthorized");
+    }
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpSession session) {
+        session.invalidate();
+        return ResponseEntity.ok()
+                .body("Logged out");
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody User user) {
+        // ... handle registration
+        User existingUser = userService.findByEmail(user.getEmail());
+        if (existingUser != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Username already exists");
+        }
+        userService.save(user);
+        UserDTO userDTO = new UserDTO(user);
+        return ResponseEntity.ok(userDTO);
+    }
 }
-
-
