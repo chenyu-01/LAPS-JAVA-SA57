bb18f8e622b14be2d127bb08051e6916a7264fea
chenyu-01 <chenyu101@outlook.com>
2023-12-16 22:24:29 +0800

    rename services

--- src/main/java/com/laps/backend/controllers/LeaveApplicationController.java
+++ src/main/java/com/laps/backend/controllers/LeaveApplicationController.java
@@ -1,8 +1,6 @@
 package com.laps.backend.controllers;
 
-import com.laps.backend.services.LeaveApplicationService;
-import com.laps.backend.services.LeaveTypeService;
-import com.laps.backend.services.UserService;
+import com.laps.backend.services.LeaveApplicationServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/leave-applications")
 public class LeaveApplicationController {
     @Autowired
-    private LeaveApplicationService leaveApplicationService;
+    private LeaveApplicationServiceImpl leaveApplicationServiceImpl;
 
     // RESTful endpoints for managing leave applications
 }
