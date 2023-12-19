f9a94ce42f9f408fb259256d8ac4ddb798c8a5ed
chenyu-01 <chenyu101@outlook.com>
2023-12-19 16:36:46 +0800

    update status

--- src/main/java/com/laps/backend/models/LeaveApplication.java
+++ src/main/java/com/laps/backend/models/LeaveApplication.java
@@ -23,8 +23,8 @@ public class LeaveApplication {
     private Date endDate;
     private String type; // e.g., Annual, Medical, Compensation, etc.
     @Column(nullable = false)
-    @Pattern(regexp = "Applied|Approved|Rejected|Deleted|Cancelled", message = "Status must be Applied, Approved, Rejected, or Deleted or Cancelled")
-    private String status; // e.g., Applied, Approved, Rejected, Deleted, Cancelled
+    @Pattern(regexp = "Applied|Approved|Rejected|Deleted|Cancelled|Updated", message = "Status must be Applied, Approved, Rejected, Deleted, Cancelled, or Updated")
+    private String status; // e.g., Applied, Approved, Rejected, Deleted, Cancelled，Updated
     private String comment; // comment for the leave application
     private String reason; // Reason for the leave
 
