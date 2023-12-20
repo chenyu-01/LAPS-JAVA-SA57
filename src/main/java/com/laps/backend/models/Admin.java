f43a246b9d556621771267382f168d008f3e1b42
chenyu-01 <chenyu101@outlook.com>
2023-12-21 02:21:05 +0800

    add constructers to convert Other Users to Admin

--- src/main/java/com/laps/backend/models/Admin.java
+++ src/main/java/com/laps/backend/models/Admin.java
@@ -11,5 +11,28 @@ public class Admin extends User {
         super();
     }
 
+    public Admin(User user) {
+        super();
+        this.setName(user.getName());
+        this.setEmail(user.getEmail());
+        this.setPassword(user.getPassword());
+        this.setRole("Admin");
+    }
+
+    public Admin(Employee employee) {
+        super();
+        this.setName(employee.getName());
+        this.setEmail(employee.getEmail());
+        this.setPassword(employee.getPassword());
+        this.setRole("Admin");
+    }
+
+    public Admin(Manager manager) {
+        super();
+        this.setName(manager.getName());
+        this.setEmail(manager.getEmail());
+        this.setPassword(manager.getPassword());
+        this.setRole("Admin");
+    }
 
 }
