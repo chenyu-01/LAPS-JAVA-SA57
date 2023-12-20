181b196594a223e141b16470c4f8fd8204583ff9
chenyu-01 <chenyu101@outlook.com>
2023-12-20 18:16:38 +0800

    change Employee Manager

--- src/main/java/com/laps/backend/models/Employee.java
+++ src/main/java/com/laps/backend/models/Employee.java
@@ -1,5 +1,6 @@
 package com.laps.backend.models;
 
+import com.laps.backend.controllers.AdminController;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -20,5 +21,31 @@ public class Employee extends User{
     private List<LeaveApplication> leaveApplications;
     public Employee() {
         super();
+    }
+
+    // constructor for converting User to Employee
+    public Employee(User user) {
+        super();
+        this.setName(user.getName());
+        this.setEmail(user.getEmail());
+        this.setPassword(user.getPassword());
+        this.setRole("Employee");
+    }
+
+    // constructor for converting Manager to Employee
+    public Employee(Manager manager) {
+        super();
+        this.setName(manager.getName());
+        this.setEmail(manager.getEmail());
+        this.setPassword(manager.getPassword());
+        this.setRole("Employee");
+    }
+
+    public Employee(Admin admin) {
+        super();
+        this.setName(admin.getName());
+        this.setEmail(admin.getEmail());
+        this.setPassword(admin.getPassword());
+        this.setRole("Employee");
     }
 }
