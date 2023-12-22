312b9d3c53ac731898179c5e5dd173c73d18be45
chenyu-01 <chenyu101@outlook.com>
2023-12-23 03:18:07 +0800

    add more ports

--- src/main/java/com/laps/backend/config/WebConfig.java
+++ src/main/java/com/laps/backend/config/WebConfig.java
@@ -6,10 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    // for dev only, allow localhost to access the backend
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins("http://localhost:5173", "http://localhost:5837", "http://localhost:8080")
                 .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
