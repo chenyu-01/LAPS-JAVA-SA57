87480aa4d5a5655cea403f31370e089c28bc196f
chenyu-01 <chenyu101@outlook.com>
2023-12-25 18:35:09 +0800

    Changes by ROG

--- src/main/java/com/laps/backend/controllers/LeaveApplicationController.java
+++ src/main/java/com/laps/backend/controllers/LeaveApplicationController.java
@@ -153,7 +153,7 @@ public class LeaveApplicationController {
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> updateEmployeeApplication(@ModelAttribute("leaveApplicationBinder") @RequestBody @Valid LeaveApplication leaveApplicationBody, BindingResult result) {
+    public ResponseEntity<?> updateEmployeeApplication(@RequestBody @Valid LeaveApplication leaveApplicationBody, BindingResult result) {
         Map<String , Object> response = new HashMap<>();
         if (result.hasErrors()) {
             // Handle validation errors
@@ -218,7 +218,7 @@ public class LeaveApplicationController {
     }
 
     @PostMapping("/submit/{id}")
-    public ResponseEntity<?> submitEmployeeApplication(@PathVariable("id") Long inid,@ModelAttribute("leaveApplicationBinder") @RequestBody @Valid LeaveApplication leaveApplicationBody, BindingResult result) {
+    public ResponseEntity<?> submitEmployeeApplication(@PathVariable("id") Long inid, @RequestBody @Valid LeaveApplication leaveApplicationBody, BindingResult result) {
         Map<String, Object> response = new HashMap<>();
         if (result.hasErrors()) {
             // Handle validation errors
